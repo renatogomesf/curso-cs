181a3d3f7f3b2a02c0e0b3e9fc0e3344e56a4da9
Renato Gomes <renato_gfe@hotmail.com>
2026-03-25 10:18:49 -0300

    aula03

--- aula03/Program.cs
+++ aula03/Program.cs
@@ -10,5 +10,18 @@ public class MyClass
         char letra = 'a';
         float valor = 5.3f;
         string nome = "bruno";
+
+        var aux = 10; // O compilador irá inferir o tipo da variável, nesse caso, int
+
+        Console.WriteLine("valor de var: " + aux);
+
+        int num1, num2, resultado;
+        
+        int num3 = 10, num4 = 10, resultado2;
+
+        num1 = 10;
+        num2 = 10;
+        resultado = num1 * num2;
+        Console.WriteLine("valor de resultado: " + resultado);
     }
 }
