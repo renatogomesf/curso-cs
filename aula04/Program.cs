43df4b9a6247c09e8a1fd5b5f1eed9deac463303
Renato Gomes <renato_gfe@hotmail.com>
2026-03-25 10:30:02 -0300

    aula04

---
+++ aula04/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace aula04;
+
+public class Aula04
+{
+
+    static int num = 10; // variável de instância/global, pode ser acessada por todos os métodos da classe MyClass
+
+
+    public static void Main()
+    {
+        int num1 = 10; // variável local do metodo Main, só pode ser acessada dentro do método Main
+
+        Console.WriteLine(num);
+    }
+
+
+    void Teste() 
+    {
+        int num2 = 1000; // variável local do método Teste, só pode ser acessada dentro do método Teste
+
+        Console.WriteLine(num2);
+    }
+}
