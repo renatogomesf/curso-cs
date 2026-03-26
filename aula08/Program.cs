b6f3017e613ae4b3e59c551f9129214076e79e42
Renato Gomes <renato_gfe@hotmail.com>
2026-03-26 17:57:16 -0300

    aula08

---
+++ aula08/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace aula08;
+
+public class Aula08
+{
+    public static void Main()
+    {
+        int v1, v2, soma;
+        string nome;
+
+
+        Console.Write("digite seu nome: ");
+        nome = Console.ReadLine(); // lê uma linha de texto do console e retorna como string
+
+        // "int.Parse": Convertendo a string lida para um inteiro
+        Console.Write("digite valor 1: ");
+        v1 = int.Parse(Console.ReadLine());
+
+        // "Convert.ToInt32": Outra forma de converter a string lida para um inteiro
+        Console.Write("digite valor 2: ");
+        v2 = Convert.ToInt32(Console.ReadLine());
+
+        soma = v1 + v2;
+
+        Console.WriteLine("nome: {0}", nome);
+        Console.WriteLine("soma: {0}", soma);
+
+    }
+}
