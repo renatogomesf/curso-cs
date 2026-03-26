8d17a734b2c1e148c7f012a2bbff045cef46bb61
Renato Gomes <renato_gfe@hotmail.com>
2026-03-26 17:38:00 -0300

    aula07

---
+++ aula07/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace aula07;
+
+public class Aula07
+{
+    public static void Main()
+    {
+        // constantes são valores que não podem ser alterados
+        const string canal = "cfb cursos";
+        const double pi = 3.14;
+
+        Console.WriteLine("canal {0}\nPi: {1}", canal, pi);
+    }
+}
