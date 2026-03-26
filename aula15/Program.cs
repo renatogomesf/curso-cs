b1790222063e26a145caada4bc657a2cbf78383a
Renato Gomes <renato_gfe@hotmail.com>
2026-03-26 18:58:07 -0300

    aula15

---
+++ aula15/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace aula15;
+
+public class Aula15
+{
+    public static void Main()
+    {
+        int tempo = 0;
+        char escolha = 'a';
+
+        switch (escolha)
+        {
+            case 'a':
+            case 'A':
+                tempo = 50;
+                break;
+
+            case 'b':
+            case 'B':
+                tempo = 480;
+                break;
+
+            case 'c':
+            case 'C':
+                tempo = 660;
+                break;
+
+            default:
+                tempo = -1;
+                break;
+        }
+
+        Console.WriteLine($"O tempo de espera é: {tempo}");
+    }
+}
