9d25c677cd1707ba0254df19bb9165b3c475d92d
Renato Gomes <renato_gfe@hotmail.com>
2026-03-27 09:08:21 -0300

    aula22

---
+++ aula22/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace aula22;
+
+public class MyClass
+{
+    public static void Main()
+    {
+        // FOR EACH
+
+        int[] num = new int[5] { 10, 20, 30, 40, 50 };
+
+        // com for normal
+        for (int i = 0; i < num.Length; i++)
+        {
+            Console.WriteLine(num[i]);
+        }
+
+        // com for each
+        // pega elemento por elemento do array e armazena na variável "item".
+        foreach (int item in num)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
