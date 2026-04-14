de6ef95ee59348ba8e4e1f751398e4df3e7139bf
Renato Gomes <renato_gfe@hotmail.com>
2026-04-14 16:40:18 -0300

    aula51 -  argumentos de entrada do programa

---
+++ aula51 - argumentos de entrada do programa/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace aula51;
+
+public class Aula51
+{
+    // Argumenntos de entrada do programa: são os valores passados para o programa quando ele é executado.
+    // com o programa já compilado, basta chamar o nome do arquivo no terminal e, em seguida, os argumentos separados por espaço (cada separação por espaço é um argumento novo). Exemplo: "aula51.exe CFB Cursos"
+    // string[] args: é um array de strings que recebe os argumentos de entrada do programa.
+    public static void Main(string[] args)
+    {
+
+        if (args.Length > 0)
+        {
+            Console.WriteLine($"Argumentos passados: {args.Length}");
+
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        else
+        {
+            Console.WriteLine("não foram passados argumentos");
+        }
+
+        Console.ReadLine();
+    }
+}
