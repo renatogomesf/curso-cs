cec8e419fbc702d51d151fd838769749a5628ff2
Renato Gomes <renato_gfe@hotmail.com>
2026-04-16 20:23:26 -0300

    aula52 - excecoes(try catch finally) P1

---
+++ aula52 - excecoes(try catch finally) P1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+// aula52 - excecoes(try catch finally) P1
+
+namespace aula52;
+
+public class Aula52
+{
+    public static void Main()
+    {
+        int n1, n2, res;
+
+        res = n1 = n2 = 0;
+
+        n1 = 10;
+        n2 = 0;
+
+        try
+        {
+            res = n1 / n2;
+            Console.WriteLine(res);
+
+        } // todas excecoes herdam da classe System.Exception
+        catch (Exception e) // pode ser qualquer tipo de excecao, mas o mais recomendado é usar a classe mais especifica possivel
+        {
+            Console.WriteLine($"ERRROOO: {e.Message}");
+            Console.WriteLine($"ERRROOO: {e.GetType()}");
+        }
+
+        Console.WriteLine("----------------------------------------------------------");
+
+        try
+        {
+            res = n1 / n2;
+            Console.WriteLine(res);
+
+        } // executa o catch mais especifico, ou seja, o que tem a classe mais especifica, nesse caso, o DivideByZeroException
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine($"ERRROOO: {e}");
+            Console.WriteLine($"ERRROOO: {e.Message}");
+            Console.WriteLine($"ERRROOO: {e.GetType()}");
+        }
+    }
+}
